7da2cdb9f1d60d4346a51a2a5e8038c4334a1936
blackpunk <blackpunk1337@gmail.com>
2019-04-17 05:48:55 +0700

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
+#include "tubes.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;
     return 0;
 }
---
+++ tubes.h
@@ -0,0 +1,13 @@
+//
+// Created by jesbuz on 17/04/19.
+//
+
+#ifndef TUBES_STD_TUBES_H
+#define TUBES_STD_TUBES_H
+
+#include <iostream>
+using namespace std;
+
+
+
+#endif //TUBES_STD_TUBES_H
