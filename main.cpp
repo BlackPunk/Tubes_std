1c49704bee9399126f1898ad9b3aa3a517637dca
Ammar A <48810314+BlackPunk@users.noreply.github.com>
2019-04-27 02:21:59 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,23 +3,24 @@
 int main() {
     multiList L;
     createList(L);
-    int Pil= -1;
-    while (Pil != 99){
+    int Pil;
+    do{
         system("CLS");
-        cout<<"+------------------------------------------------------------------+\n";
-        cout<<"|                    *  SISTEM PERKULIAHAN  *                      |\n";
-        cout<<"+------------------------------------------------------------------+\n";
-        cout<<"| 1. Tambahkan Mata Kuliah                                         |\n";
-        cout<<"| 2. Tambahkan Dosen                                               |\n";
-        cout<<"| 3. Hubungkan Dosen dengan Mata Kuliah                            |\n";
-        cout<<"| 4. Hapus Mata Kuliah                                             |\n";
-        cout<<"| 5. Hapus Dosen                                                   |\n";
-        cout<<"| 6. Menampilkan semua Mata kuliah beserta dosennya                |\n";
-        cout<<"| 7. Menampilkan Mata kuliah yang diajarkan sebagian besar Dosen   |\n";
-        cout<<"| 8. Menampilkan Dosen yang mengajar mata kuliah tertentu          |\n";
-        cout<<"| 9. Menampilkan Dosen yang mengajar lebih dari tiga kursus        |\n";
-        cout<<"| 99. Keluar                                                       |\n";
-        cout<<"+__________________________________________________________________+\n";
+        cout<<"+-------------------------------------------------------------------+\n";
+        cout<<"|                    *  SISTEM PERKULIAHAN  *                       |\n";
+        cout<<"+-------------------------------------------------------------------+\n";
+        cout<<"| 1.  Tambahkan Matakuliah                                          |\n";
+        cout<<"| 2.  Tambahkan Dosen                                               |\n";
+        cout<<"| 3.  Hubungkan Dosen dengan Matakuliah                             |\n";
+        cout<<"| 4.  Hapus Mata Kuliah                                             |\n";
+        cout<<"| 5.  Hapus Dosen                                                   |\n";
+        cout<<"| 6.  Menampilkan semua Matakuliah beserta dosennya                 |\n";
+        cout<<"| 7.  Menampilkan Matakuliah yang diajarkan sebagian besar Dosen    |\n";
+        cout<<"| 8.  Menampilkan Dosen yang mengajar Matakuliah tertentu           |\n";
+        cout<<"| 9.  Menampilkan Dosen yang mengajar lebih dari tiga kursus        |\n";
+        cout<<"| 10. Hapus relasi                                                  |\n";
+        cout<<"| 99. Keluar                                                        |\n";
+        cout<<"+___________________________________________________________________+\n";
         cout<<"\nMasukkan Pilihan : ";cin>>Pil;
         switch (Pil){
             case 1:
@@ -38,7 +39,10 @@ int main() {
                 HapusDosen(L);
                 break;
             case 6:
+                system("CLS");
+                cout<<"+------------- DAFTAR MATAKULIAH BESERTA DOSEN PENGAJARNYA -------------+\n\n";
                 TampilMataKuliahDanDosennya(L);
+                cout<<"\n\nEnter untuk kembali...";
                 getch();
                 break;
             case 7:
@@ -50,12 +54,15 @@ int main() {
             case 9:
                 tampilDosen3Matkul(L);
                 break;
+            case 10:
+                menuHapusRelasi(L);
+                break;
             case 99:
                 break;
             default:
                 cout<<"Pilihan tidak ada...\n";
                 getch();
         }
-    }
+    }while (Pil!=99);
     return 0;
 }
